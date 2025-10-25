efb88aded9f57bb9f85d4b9f33eabaa67e6b268f
VentsiKanev <160255023+VentsiKanev@users.noreply.github.com>
2025-10-25 15:00:19 +0300

    Added validation check for str and SWITCH

--- Helper.py
+++ Helper.py
@@ -2,12 +2,15 @@ def validation_user_type_int(user_input_element):
     if type(user_input_element) not in (int, float):
         return 
     elif user_input_element > 0:
-        calc_result = result(user_input_element)
-        print(calc_result)
+        return user_input_element
+        #calc_result = result(user_input_element)
+        #print(calc_result)
+    elif user_input_element >= 5:
+        print("The only acceptable numbers are between 1 to 4")
     elif user_input_element == 0:
         print("Cannot be zero")
     elif user_input_element < 0:
-        print("Cannot be negative value")
+        print("\nCannot be negative value")
 
 calc_to_hours = 24
 calc_to_minutes = 1440
@@ -16,8 +19,10 @@ def result(num_of_days):
     return f"{num_of_days} is {num_of_days * calc_to_hours} hours and {num_of_days * calc_to_minutes} minutes"
 
 
-def validation_user_type_string(user_input_element):
-    if not user_input_element.isalpha():  # NEW: Check if only letters
+def validation_user_type_string(user_input_element, allow_special_chars=False):
+    #adding a switch to turn ON (True) and OFF (False) to allow_special_characters
+   
+    if not allow_special_chars and not user_input_element.isalpha():
         print("Error: Input must contain only alphabetical letters")
         return 
     elif len(user_input_element) == 0:
